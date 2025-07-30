03352522cc800fbd34f9b9b27155492e8d44ef6f
Ernando Rezende <ernando.03@gmail.com>
2025-07-30 22:57:16 +0100

    Replace System.out.println by Logger

--- src/main/java/br/com/ernando/sockets/chat/Server.java
+++ src/main/java/br/com/ernando/sockets/chat/Server.java
@@ -1,16 +1,21 @@
 package br.com.ernando.sockets.chat;
 
 import javax.swing.*;
+
+import jline3.terminal.src.main.java.org.jline.utils.Log;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Logger;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        System.out.println("Starting server...");
+        Logger logger = Logger.getLogger(Server.class.getName());
+        logger.info("Starting server...");
         ServerSocket serverSocket = new ServerSocket(5001);
         Socket socketClient = serverSocket.accept();
         PrintWriter out = new PrintWriter(socketClient.getOutputStream(), true);
@@ -20,7 +25,7 @@ public class Server {
         String input;
         while (true) {
             input = JOptionPane.showInputDialog(null, "Messagem to client", "Server", JOptionPane.INFORMATION_MESSAGE);
-            System.out.println("Server:" + input);
+            logger.info("Server: {}", input);
             out.println(input);
         }
     }
